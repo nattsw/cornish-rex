a082c2706cff62cea8827cf142f838f2def5bad5
Natalie Tay <natalie.tay@gmail.com>
2017-11-22 22:41:46 +0800

    Initial commit of barebones spring boot app

---
+++ src/main/java/io/natalietay/conways/ConwaysApplication.java
@@ -0,0 +1,12 @@
+package io.natalietay.conways;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConwaysApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConwaysApplication.class, args);
+	}
+}
---
+++ src/test/java/io/natalietay/conways/ConwaysApplicationTests.java
@@ -0,0 +1,16 @@
+package io.natalietay.conways;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ConwaysApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
